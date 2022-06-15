3c0a3bd5c012f4db68a0cf1e7ec3f37c5dc4b472
BORI <84188904+250b@users.noreply.github.com>
2022-06-15 14:21:31 +0900

    Add files via upload

---
+++ SnakeGame.h
@@ -0,0 +1,54 @@
+#include <ncurses.h>
+#include <cstdlib>
+#include <vector>
+#include "Color.h"
+#include "Constant.h"
+#include "Snake.h"
+using namespace std;
+
+struct Item
+{
+	int x;
+	int y;
+	bool Growth;
+};
+
+struct Mission
+{
+	int maxLength;
+	bool isMaxLength;
+	int maxGrowth;
+	bool isMaxGrowth;
+	int maxPoison;
+	bool isMaxPoison;
+	int maxGate;
+	bool isMaxGate;
+};
+
+class SnakeGame
+{
+private:
+	Mission mission[4];
+	Snake snake;
+	Gate gate[2];
+	Item itemList[2];
+
+	int stage;
+	int map[4][MAX][MAX];
+	vector<pair<int, int>> wallList[4];
+
+	int itemTimer;
+	int gateTimer;
+
+public:
+    SnakeGame();
+		~SnakeGame();
+
+	bool checkMissionClear();
+	void setGate(int Map[MAX][MAX]);
+	void resetItems(int Map[MAX][MAX]);
+	void setItem(int Map[MAX][MAX]);
+	void renderMap(int Map[MAX][MAX]);
+	void renderBlock(int color);
+	void gameStart();
+};
